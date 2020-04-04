7ca1d97b89d69f42c53eea6754dc9ef3dcc476af
bergony <bergony@hotmail.com>
2020-04-04 02:41:34 -0300

    Web wins

--- src/app.js
+++ src/app.js
@@ -5,30 +5,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser');
-
 var router = express.Router();
 
 
-var indexRouter = require('./routes/index.js');
-var usersRouter = require('./routes/users.js');
-var seacherRouter = require('./routes/seacher');
+var indexRouter = require('../routes/index.js');
+var usersRouter = require('../routes/users.js');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/s', seacherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,6 +46,7 @@ app.use(function(err, req, res, next) {
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 
 module.exports = app;
