89572f98133d8be603c30b62a42072deb772f77c
bergony <bergony@hotmail.com>
2020-04-04 03:30:02 -0300

    email fixed

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
-    res.status(err.status || 500);
+    res.status(err.status || 500);F
     res.render('error');
 });
 
